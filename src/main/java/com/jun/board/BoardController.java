b6c834d69ed492aba00650bc0e852e6a5a6eb421
jkjan <ytom2607@gmail.com>
2022-03-22 17:17:20 +0900

    로깅 연습

--- src/main/java/com/jun/board/BoardController.java
+++ src/main/java/com/jun/board/BoardController.java
@@ -1,6 +1,6 @@
 package com.jun.board;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Controller
+@Slf4j
 public class BoardController {
     final private BoardService boardService;
 
@@ -22,17 +23,25 @@ public class BoardController {
         ModelAndView mv = new ModelAndView("boardList");
         List<Board> list = boardService.selectBoardList();
         mv.addObject("list", list);
+        log.trace("Trace");
+        log.debug("Debug");
+        log.info("Info");
+        log.warn("Warn");
+        log.error("Error");
+        log.info("Accessing Board List");
         return mv;
     }
 
     @RequestMapping("/board/openBoardWrite.do")
     public String openBoardWrite() throws Exception {
+        log.info("Writing a new post");
         return "boardWrite";
     }
 
     @RequestMapping("/board/insertBoard.do")
     public String insertBoardWrite(Board board) throws Exception {
         boardService.insertBoard(board);
+        log.info("Wrote a new post, redirecting to Board List");
         return "redirect:/board/openBoardList.do";
     }
 
@@ -41,18 +50,21 @@ public class BoardController {
         ModelAndView mv = new ModelAndView("boardDetail");
         Optional<Board> board = boardService.selectBoardDetail(boardIdx);
         mv.addObject("board", board.orElse(null));
+        log.info("Reading a post");
         return mv;
     }
 
     @RequestMapping("/board/deleteBoard.do")
     public String deleteBoard(@RequestParam int boardIdx) throws Exception {
         boardService.deleteBoard(boardIdx);
+        log.info("Deleted a post, redirecting to Board List");
         return "redirect:/board/openBoardList.do";
     }
 
     @RequestMapping("/board/updateBoard.do")
     public String updateBoard(Board board) throws Exception {
         boardService.updateBoard(board);
+        log.info("Updated a post, redirecting to Board List");
         return "redirect:/board/openBoardList.do";
     }
 }
