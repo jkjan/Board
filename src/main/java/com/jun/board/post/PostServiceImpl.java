64538ce56e776f21d3e5c9435fda7d0138b084cc
jkjan <ytom2607@gmail.com>
2022-03-24 15:43:57 +0900

    조회수 증가 Transaction으로 변경

--- src/main/java/com/jun/board/post/PostServiceImpl.java
+++ src/main/java/com/jun/board/post/PostServiceImpl.java
@@ -1,11 +1,14 @@
 package com.jun.board.post;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class PostServiceImpl implements PostService {
     final private PostRepository postRepository;
@@ -26,13 +29,14 @@ public class PostServiceImpl implements PostService {
     }
 
     @Override
+    @Transactional
     public Optional<Post> getPost(int postIdx) {
         Optional<Post> ret = postRepository.findById(postIdx);
 
         // 조회수 증가
         if (ret.isPresent()) {
             ret.get().setHitCnt(ret.get().getHitCnt() + 1);
-            postRepository.save(ret.get());
+            log.debug("Add 1 to hit cnt");
         }
 
         return ret;
